0b30eb4fc1d51bf18ba8709a567460145c1e0d30
priscilablf <142513652+priscilablf@users.noreply.github.com>
2023-08-31 11:25:43 -0300

    Add files via upload

---
+++ andre/listasetc.py
@@ -0,0 +1,10 @@
+lista = []
+tupla = ()
+dicionario = {}
+dicionario ["Professores"] = ["Andre", "Denis", "Carol"]
+dicionario ["Alunos"] = ["Priscila"]
+print(dicionario)
+dicionario = {
+    'Professores': ['Andre','Denis', 'Carol'],
+    'Alunos': ['Priscila'] 
+}

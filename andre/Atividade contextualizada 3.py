112b9d1eca7b82369e42dc35382181d020d52b77
Priscila Barbosa Lins Falcão <142513652+priscilablf@users.noreply.github.com>
2023-09-17 16:58:42 -0300

    Add files via upload

---
+++ andre/Atividade contextualizada 3.py
@@ -0,0 +1,210 @@
+zoom = 40 #%
+rotacao = 122 #°
+cor = 3 #verde (0=preto; 1=branco; 2=amarelo; 3= verde; 4=azul)
+velocidade_varredura= 100 #Hz
+potencia = 50 #%
+pixel= 520
+tamanho_imagem = 250 #µm
+formato = "500x500"
+varredura = 1
+uv = 30 #%
+
+print ("Esse programa tem função de configurar um microscópio conscopal para análise de imagens histológicas em alta resolução")
+
+zoom = int(input("Qual o zoom do microscópio?"))
+print("Houve alguma alteração?")
+print(zoom != 40)
+
+rotacao = int(input("Em que rotação está o microscópio?"))
+print("Houve alguma alteração?")
+print(rotacao != 122)
+
+cor = int(input("Qual a cor da imagem?"))
+print("Houve alguma alteração?")
+print(cor != 3)
+
+velocidade_varredura = int(input("Qual a velocidade de varredura?"))
+print("Houve alguma alteração?")
+print(velocidade_varredura != 100)
+
+potencia = int(input("Qual a potência mensurada?"))
+print("Houve alguma alteração?")
+print(potencia != 50)
+
+pixel = int(input("Quantos pixels tem na imagem?"))
+print("Houve alguma alteração?")
+print(pixel != 520)
+
+tamanho_imagem = int(input("Qual o tamanho da imagem?"))
+print("Houve alguma alteração?")
+print(tamanho_imagem != 250)
+
+formato = input("Qual é o formato da imagem?")
+print("Houve alguma alteração?")
+print(formato != "500x500")
+
+varredura = int(input("Quantas varreduras serão feitas?"))
+print("Houve alguma alteração?")
+print(varredura != 1)
+
+uv = int(input("Qual a porcentagem do UV?"))
+print("Houve alguma alteração?")
+print(uv != 30)
+
+print("As informações de configurações feitas pelo usuário são")
+print("zoom =", zoom)
+print("rotacao =", rotacao)
+print("cor=", cor)
+print("velocidade_varredura=", velocidade_varredura)
+print("potencia=", potencia)
+print("pixel=", pixel)
+print("tamanho_imagem=", tamanho_imagem)
+print("formato=", formato)
+print("varredura=", varredura)
+print("uv=", uv) 
+
+
+print("A partir de agora a calibração será na horizontal. Use a primeira e última letra do seu nome.")
+primeira_letra = "p"
+ultima_letra = "a"
+
+letra1 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra1)
+
+letra2 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra2)
+
+letra3 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra3)
+
+letra4 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra4)
+
+letra5 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra5)
+
+letra6 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra6)
+
+letra7 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra7)
+
+letra8 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra8)
+
+letra9 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra9)
+
+letra10 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra10)
+
+letra11 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra11)
+
+letra12 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra12)
+
+letra13 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra13)
+
+letra14 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra14)
+
+letra15 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra15)
+
+letra16 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra16)
+
+letra17 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra17)
+
+letra18 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra18)
+
+letra19 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra19)
+
+letra20 = input("Digite primeira ou a última letra")
+print ("informacao_correta", letra20)
+
+
+print("A partir de agora a calibração será na vertical. Use a segunda e penúltima letra do seu nome.")
+segunda_letra = "r"
+penultima_letra = "l"
+
+letra21 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra21)
+
+letra22 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra22)
+
+letra23 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra23)
+
+letra24 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra24)
+
+letra25 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra25)
+
+letra26 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra26)
+
+letra27 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra27)
+
+letra28 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra28)
+
+letra29 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra29)
+
+letra30 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra30)
+
+letra31 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra31)
+
+letra32= input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra32)
+
+letra33 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra33)
+
+letra34 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra34)
+
+letra35 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra35)
+
+letra36 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra36)
+
+letra37 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra37)
+
+letra38 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra38)
+
+letra39 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra39)
+
+letra40 = input("Digite segunda ou a penúltima letra")
+print ("informacao_correta", letra40)
+
+print ("término da calibração")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
